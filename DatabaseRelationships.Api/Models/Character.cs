043d47c7c3107e01f84cd4de23a351135e4120ec
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-13 14:43:53 +0300

    Removed the BackPackId field

--- DatabaseRelationships.Api/Models/Character.cs
+++ DatabaseRelationships.Api/Models/Character.cs
@@ -4,6 +4,5 @@ public class Character
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public int BackPackId { get; set; }
     public BackPack BackPack { get; set; } = new BackPack();
 }
