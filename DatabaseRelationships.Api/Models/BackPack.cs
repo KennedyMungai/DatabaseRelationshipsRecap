dba8751d9d75c30b56e4a4e1182a5b68bf91bfbe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-13 14:39:43 +0300

    Created the BackPack model class

--- DatabaseRelationships.Api/Models/BackPack.cs
+++ DatabaseRelationships.Api/Models/BackPack.cs
@@ -3,5 +3,8 @@ namespace DatabaseRelationships.Api.Models;
 
 public class BackPack
 {
-
+    public int Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public int CharacterId { get; set; }
+    public Character Character { get; set; } = new Character();
 }
