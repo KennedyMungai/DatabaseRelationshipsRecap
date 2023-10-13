8700a488cf427e64c2dfa91cb171d039309bb93e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-13 14:38:14 +0300

    Created the Character model file

--- DatabaseRelationships.Api/Models/Character.cs
+++ DatabaseRelationships.Api/Models/Character.cs
@@ -2,5 +2,8 @@ namespace DatabaseRelationships.Api.Models;
 
 public class Character
 {
-
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int BackPackId { get; set; }
+    public BackPack BackPack { get; set; } = new BackPack();
 }
