58701cd04defa08e0edefc57f6b6c6f916288a95
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-13 15:36:46 +0300

    Added the faction as a property for the Character

--- DatabaseRelationships.Api/Models/Character.cs
+++ DatabaseRelationships.Api/Models/Character.cs
@@ -6,4 +6,5 @@ public class Character
     public string Name { get; set; } = string.Empty;
     public BackPack BackPack { get; set; } = new BackPack();
     public List<Weapon> Weapons { get; set; } = new List<Weapon>();
+    public List<Faction> Factions { get; set; } = new List<Faction>();
 }
