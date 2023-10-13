f75b0b9feeb2d5a032220f0269c5bae8e4a88fdc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-13 15:13:15 +0300

    Added the List of Weapons for the character

--- DatabaseRelationships.Api/Models/Character.cs
+++ DatabaseRelationships.Api/Models/Character.cs
@@ -5,4 +5,5 @@ public class Character
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public BackPack BackPack { get; set; } = new BackPack();
+    public List<Weapon> Weapons { get; set; } = new List<Weapon>();
 }
