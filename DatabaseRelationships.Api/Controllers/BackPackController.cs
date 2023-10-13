08049855f2d77a8603b83d386d8d9c82b5b5ee55
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-13 16:25:18 +0300

    Created the endpoint to create a character in the database

--- DatabaseRelationships.Api/Controllers/BackPackController.cs
+++ DatabaseRelationships.Api/Controllers/BackPackController.cs
@@ -1,5 +1,8 @@
 using AutoMapper;
 using DatabaseRelationships.Api.Data;
+using DatabaseRelationships.Api.Models;
+using DatabaseRelationships.Api.Models.DTOs;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatabaseRelationships.Api.Controllers;
@@ -15,5 +18,22 @@ public class BackPackController : ControllerBase
     {
         _context = context;
         _mapper = mapper;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<List<Character>>> CreateCharacter(CharacterCreateDTO character)
+    {
+        if (!ModelState.IsValid)
+        {
+            return await Task.FromResult(BadRequest(ModelState));
+        }
+
+        await _context.Characters.AddAsync(_mapper.Map<Character>(character));
+
+        await _context.SaveChangesAsync();
+
+        return await Task.FromResult(CreatedAtRoute("RetrieveOneCharacter", new { Id = _mapper.Map<Character>(character).Id }, character));
     }
 }
