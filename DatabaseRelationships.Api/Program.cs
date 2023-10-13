2a5a44c067c6b8e5d42d5f1f44ff0a786f1a994c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-13 14:34:31 +0300

    Registered the DbContext for the application

--- DatabaseRelationships.Api/Program.cs
+++ DatabaseRelationships.Api/Program.cs
@@ -1,3 +1,6 @@
+using DatabaseRelationships.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BackPackContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnString"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnString"))
+    );
+});
 
 var app = builder.Build();
 
