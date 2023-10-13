9f866b16a724ef1fb364efc4dae98533014fd6f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-13 15:41:38 +0300

    Data Migrations files for the completed database

--- DatabaseRelationships.Api/Data/Migrations/20231013124052_AddedFactionsToDbSet.Designer.cs
+++ DatabaseRelationships.Api/Data/Migrations/20231013124052_AddedFactionsToDbSet.Designer.cs
@@ -2,6 +2,7 @@
 using DatabaseRelationships.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatabaseRelationships.Api.Data.Migrations
 {
     [DbContext(typeof(BackPackContext))]
-    partial class BackPackContextModelSnapshot : ModelSnapshot
+    [Migration("20231013124052_AddedFactionsToDbSet")]
+    partial class AddedFactionsToDbSet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
